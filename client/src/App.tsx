b5556e5bf5fc9378d9e55c576fd78722c775865d
metaJT <jordantrotter3@gmail.com>
2026-04-15 20:25:29 -0500

    feat: updating port & refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
